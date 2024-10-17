0d9cc9c8456887d7055703870b600400eebc2db6
Evaldo Cunha F <evaldocunhaf@gmail.com>
2024-10-17 18:54:23 -0300

    Commit inicial do projeto, com começo de conexão ao banco de dados

---
+++ projetoBD/restaurante_admin/src/main/java/br/com/guest/restaurante_admin/RestauranteAdminApplication.java
@@ -0,0 +1,13 @@
+package br.com.guest.restaurante_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestauranteAdminApplication.class, args);
+    }
+
+}
---
+++ projetoBD/restaurante_admin/src/test/java/br/com/guest/restaurante_admin/RestauranteAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guest.restaurante_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
