0cd2350731e55ff67b8502a16beb695987d66bf7
Evaldo Cunha F <evaldocunhaf@gmail.com>
2024-11-18 19:51:04 -0300

    ajustes bem minimalistas

--- projetoBD/restaurante_admin/src/main/java/br/com/guest/restaurante_admin/entidades/dashboard/impl/DashboardRepositoryImpl.java
+++ projetoBD/restaurante_admin/src/main/java/br/com/guest/restaurante_admin/entidades/dashboard/impl/DashboardRepositoryImpl.java
@@ -55,7 +55,7 @@ public class DashboardRepositoryImpl implements DashboardRepository {
 
     @Override
     public List<Map<String, Object>> produtosProximosAValidade() {
-        String sql = "SELECT P.nome as prato, DATEDIFF(P.validade, DATE(NOW())) as dias_para_estragar FROM Produto P ORDER BY DATEDIFF(P.validade, DATE(NOW())) LIMIT 10";
+        String sql = "SELECT P.nome as prato, DATEDIFF(DATE(NOW()), P.validade) as dias_para_estragar FROM Produto P ORDER BY DATEDIFF(P.validade, DATE(NOW())) LIMIT 10";
         return jdbcTemplate.queryForList(sql);
     }
 
